0de0471a227464981bec0a6feb31ea7f58eb06dd
cx9ps3 <cx9ps3@gmail.com>
2023-05-13 17:55:30 +0300

    added zooming

--- src/fractals/Mandelbrot.cpp
+++ src/fractals/Mandelbrot.cpp
@@ -4,8 +4,9 @@
 
 #include "Mandelbrot.hpp"
 #include "GL/glew.h"
-
-Mandelbrot::Mandelbrot(int windowWidth, int windowHeight, const std::string &vertexShaderPath,const std::string &fragmentShaderPath)
+#include <random>
+#include <thread>
+Mandelbrot::Mandelbrot(int windowWidth, int windowHeight, const std::string &vertexShaderPath,const std::string &fragmentShaderPath):windowHeight(windowHeight),widowWidth(windowWidth)
 {
     shaderProgram = std::make_unique<ShaderProgram>(vertexShaderPath,fragmentShaderPath);
     VAO.bind();
@@ -15,15 +16,38 @@ Mandelbrot::Mandelbrot(int windowWidth, int windowHeight, const std::string &ver
     VBO.unbind();
     VAO.unbind();
     EBO.unbind();
+    std::thread generatingThread([this](){generateRandomScalePosition();});
+    generatingThread.detach();
 }
 
 void Mandelbrot::draw() noexcept
 {
-
-    shaderProgram->setDoubleUniform("width",static_cast<double>(800));
-    shaderProgram->setDoubleUniform("height",static_cast<double>(600));
-    shaderProgram->setIntegerUniform("maxIterations",250);
+    shaderProgram->setDoubleUniform("width",static_cast<double>(widowWidth));
+    shaderProgram->setDoubleUniform("height",static_cast<double>(windowHeight));
+    shaderProgram->setIntegerUniform("maxIterations",500);
+    shaderProgram->setDoubleUniform("scaleFactor",scale);
+    shaderProgram->setIntegerUniform("randomX",randomX);
+    shaderProgram->setIntegerUniform("randomY",randomY);
     shaderProgram->use();
     VAO.bind();
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    scale -= scaleFactor;
+    if(scale < minimalScaleLimit)
+    {
+        scale = startScale;
+    }
+}
+
+void Mandelbrot::generateRandomScalePosition()
+{
+     std::random_device rd;
+     std::mt19937 mt(rd());
+     std::uniform_int_distribution<int> generateX((widowWidth / 2) - xLimit, (widowWidth / 2) + xLimit);
+     std::uniform_int_distribution<int> generateY((windowHeight / 2) - yLimit, (windowHeight / 2 ) + yLimit);
+    while(isGenerating)
+    {
+        randomX = generateX(mt);
+        randomY = generateY(mt);
+        std::this_thread::sleep_for(threadSleepTime);
+    }
 }
--- src/fractals/Mandelbrot.hpp
+++ src/fractals/Mandelbrot.hpp
@@ -9,19 +9,36 @@
 #include "../buffers/VertexBufferObject.hpp"
 #include "../shaders/ShaderProgram.hpp"
 #include "../buffers/ElementsBufferObject.hpp"
+
 #include <memory>
+#include <chrono>
+using namespace std::chrono_literals;
+
 class Mandelbrot
 {
 public:
     Mandelbrot(int windowWidth,int windowHeight,const std::string &vertexShaderPath , const std::string &fragmentShaderPath);
     void draw() noexcept;
 private:
+    void generateRandomScalePosition();
+    double startScale = 1.0;
+    double scale = startScale;
+    double scaleFactor = 0.0001;
+    double minimalScaleLimit = 0.000001;
     RectangleData rectangleData;
     VertexBufferObject VBO;
     VertexAttributes attributes;
     VertexArrayObject VAO;
     ElementsBufferObject EBO;
     std::unique_ptr<ShaderProgram> shaderProgram{nullptr};
+    std::chrono::seconds threadSleepTime = 120s;
+    int xLimit = 100;
+    int yLimit = 100;
+    std::atomic<int> randomX;
+    std::atomic<int> randomY;
+    std::atomic<bool> isGenerating{true};
+    int widowWidth;
+    int windowHeight;
 };
 
 
