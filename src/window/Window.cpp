04c1181bdf3adc7892963bdea7a145e6d860122f
cx9ps3 <cx9ps3@gmail.com>
2023-05-13 21:11:04 +0300

    refactored class added GLEW GLFW initialization in methods

--- src/window/Window.cpp
+++ src/window/Window.cpp
@@ -7,41 +7,20 @@
 
 Window::Window(bool isFullscreen)
 {
-    if(!glfwInit())
-    {
-        showErrorMessage("ERROR","Failed to initialize GLFW\n");
-        exit(-1);
-    }
 
-    glfwSetErrorCallback(glfwErrorCallback);
-
-    constexpr auto windowWidth = 800;
-    constexpr auto windowHeight = 600;
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_ANY_PROFILE);
-
-    window = glfwCreateWindow(windowWidth,windowHeight,"savescreen", nullptr, nullptr);
-
-    if(window == nullptr)
-    {
-        showErrorMessage("ERROR","Failed to open main screen");
-        exit(-1);
-    }
-    glfwMakeContextCurrent(window);
-    if(glewInit() != GLEW_OK)
-    {
-        showErrorMessage("ERROR","Failed to initialize GLEW\n");
-        exit(-1);
-    }
-    glfwSetKeyCallback(window,Window::keyCallback);
-
-    width = windowWidth;
-    height = windowHeight;
-    setFullscreen(isFullscreen);
+    initGLFW();
+    initGLFWWindow();
+    initGLEW();
+    //setFullscreen(isFullscreen);
 }
-
+#if defined(WIN32) || defined(WIN64)
+Window::Window(HWND descriptor)
+{   initGLFW();
+    initGLFWWindow();
+    window = glfwAttachWin32Window(descriptor, window);
+    initGLEW();
+}
+#endif
 Window::~Window()
 {
     glfwDestroyWindow(window);
@@ -79,12 +58,12 @@ void Window::resizeCallback(GLFWwindow *window, int width, int height)
     glfwGetWindowSize(window, &width, &height);
 }
 
-inline int Window::getWidth() const noexcept
+int Window::getWidth() const noexcept
 {
     return width;
 }
 
-inline int Window::getHeight() const noexcept
+int Window::getHeight() const noexcept
 {
     return height;
 }
@@ -103,5 +82,41 @@ void Window::setFullscreen(bool isFullscreen)
         glfwSetWindowMonitor(window, nullptr,0,0,width,height,0);
         glViewport(0,0,width,height);
 
+    }
+}
+
+void Window::initGLFW()
+{
+    if(!glfwInit())
+    {
+        showErrorMessage("ERROR","Failed to initialize GLFW\n");
+        exit(-1);
+    }
+    glfwSetErrorCallback(glfwErrorCallback);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_ANY_PROFILE);
+}
+
+void Window::initGLFWWindow()
+{
+    window = glfwCreateWindow(width,height,"Mandelbrot Screensaver", nullptr, nullptr);
+    if(window == nullptr)
+    {
+        showErrorMessage("ERROR","Failed to open main screen");
+        exit(-1);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window,Window::keyCallback);
+
+}
+
+void Window::initGLEW()
+{
+    if(glewInit() != GLEW_OK)
+    {
+        showErrorMessage("ERROR","Failed to initialize GLEW\n");
+        exit(-1);
     }
 }
--- src/window/Window.hpp
+++ src/window/Window.hpp
@@ -5,12 +5,22 @@
 #ifndef MANDELBROTSCREENSAVER_WINDOW_HPP
 #define MANDELBROTSCREENSAVER_WINDOW_HPP
 #include <GL/glew.h>
+#if defined(WIN32) || defined(WIN64)
+#include <Windows.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
+#define GLFW_NATIVE_INCLUDE_NONE
+#include <GLFW/glfw3native.h>
+#endif
 #include <GLFW/glfw3.h>
 
 class Window
 {
 public:
     explicit Window(bool isFullscreen = false);
+#if defined(WIN32) || defined(WIN64)
+    explicit Window(HWND descriptowWindow);
+#endif
     int getWidth() const noexcept;
     int getHeight() const noexcept;
     void setFullscreen(bool isFullscreen);
@@ -19,9 +29,12 @@ public:
     bool isShouldClose();
     ~Window();
 private:
+    void initGLFW();
+    void initGLFWWindow();
+    void initGLEW();
     static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
     static void resizeCallback(GLFWwindow* window, int width, int height);
-    int width{0}, height{0};
+    int width{800}, height{600};
     GLFWwindow *window;
 };
 
